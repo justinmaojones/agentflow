1e4c214688549464198b0d76dda9d8e44723e977
Justin Mao-Jones <justinmaojones@gmail.com>
2019-09-21 13:58:38 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='rl',
+	version='1.0',
+	description='reinforcement learning project',
+	author='Justin Mao-Jones',
+	author_email='justinmaojones@gmail.com',
+	install_requires=open('requirements.txt').read(),
+	packages=[],
+ )
