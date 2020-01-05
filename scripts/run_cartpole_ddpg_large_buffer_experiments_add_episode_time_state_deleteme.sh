2111feca2eb205248e6e634ca1ef9f9121e86cc4
Justin Mao-Jones <justinmaojones@gmail.com>
2020-01-05 00:32:36 -0800

    remove scripts/run_cartpole_ddpg_large_buffer_experiments_add_episode_time_state_deleteme.sh

--- scripts/run_cartpole_ddpg_large_buffer_experiments_add_episode_time_state_deleteme.sh
+++
@@ -1,4 +0,0 @@
-export NUMSTEPS=100000
-export BUFFER_SIZE=20000
-
-python examples/cartpole_ddpg.py --num_steps=$NUMSTEPS --buffer_type=prioritized --buffer_size=$BUFFER_SIZE --add_episode_time_state=True --seed=1 --savedir='results/deleteme/prioritized_buffer_large_add_time_state'
