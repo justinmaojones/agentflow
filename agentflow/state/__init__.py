3f5c59ce0b7a8d949cb5ef658bc600259e30ee8b
Justin Mao-Jones <justinmaojones@gmail.com>
2020-03-04 22:43:43 -0800

    add image states

--- agentflow/state/__init__.py
+++ agentflow/state/__init__.py
@@ -1,3 +1,5 @@
 from .n_prev_frames_state import NPrevFramesStateEnv
 from .add_episode_time_state import AddEpisodeTimeStateEnv
 from .reward_forward_filter import RewardForwardFilterEnv
+from .resize_image_state import ResizeImageStateEnv
+from .cvt_rgb2gray_image_state import CvtRGB2GrayImageStateEnv
