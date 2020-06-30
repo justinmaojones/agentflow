8298968f5f3df264bb0dab14b57f455cb39f5f44
Justin Mao-Jones <justinmaojones@gmail.com>
2020-06-29 22:44:04 -0700

    update pong debug script

--- scripts/examples/pong/debug.sh
+++ scripts/examples/pong/debug.sh
@@ -1,13 +1,13 @@
-export NUMSTEPS=100000
+export NUMSTEPS=1000
 export BUFFER_SIZE=50000
-export BEGIN_LEARNING_AT_STEP=3000
-export N_UPDATE_STEPS=1
+export BEGIN_LEARNING_AT_STEP=0
+export N_UPDATE_STEPS=4
 export BATCHSIZE=64
 export UPDATE_FREQ=1
 export N_PREV_FRAMES=12
 export LEARNING_RATE_Q=1e-1
 export CONV_DIMS=64
-export HIDDEN_DIMS=16
+export HIDDEN_DIMS=128
 export HIDDEN_LAYERS=2
 
 export SAVEDIR=results/pong_ddpg_stable_debug/lrq${LEARNING_RATE_Q}_npf${N_PREV_FRAMES}_momentum0.2_nesterov_alpha2_prioritized_init_q5k_wd1e-3_cd${CONV_DIMS}_hd${HIDDEN_DIMS}_hl${HIDDEN_LAYERS}
@@ -19,6 +19,7 @@ do
         --num_steps=$NUMSTEPS \
         --begin_learning_at_step=$BEGIN_LEARNING_AT_STEP \
         --n_update_steps=$N_UPDATE_STEPS \
+        --n_prev_frames=32 \
         --batchsize=$BATCHSIZE \
         --binarized=True \
         --batchnorm=True \
@@ -31,7 +32,7 @@ do
         --noisy_action_prob=0.1 \
         --learning_rate=0.1 \
         --learning_rate_q=$LEARNING_RATE_Q \
-        --n_steps_train_only_q 1000 \
+        --n_steps_train_only_q 0 \
         --n_prev_frames=$N_PREV_FRAMES \
         --weight_decay=0.01 \
         --ema_decay=0.9 \
