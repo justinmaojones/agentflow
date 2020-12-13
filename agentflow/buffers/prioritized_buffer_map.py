eadde2935b23cec27abdadbccdfe70cb853bef97
Justin Mao-Jones <justinmaojones@gmail.com>
2020-12-12 20:41:53 -0800

    default priorities

--- agentflow/buffers/prioritized_buffer_map.py
+++ agentflow/buffers/prioritized_buffer_map.py
@@ -23,28 +23,48 @@ class PrioritizedSamplingBuffer(NDArrayBuffer):
 
 class PrioritizedBufferMap(BufferMap):
 
-    def __init__(self,max_length=2**20,alpha=0.6,eps=1e-4,wclip=32.,n_beta_annealing_steps=None):
+    def __init__(self,
+            max_length = 2**20,
+            alpha = 0.6,
+            eps = 1e-4,
+            wclip = 32.,
+            default_priority = 1.0,
+            default_non_zero_reward_priority = None,
+            default_done_priority = None,
+            ):
+
         super(PrioritizedBufferMap,self).__init__(max_length)
 
         assert alpha >= 0
         self._alpha = alpha
         self._eps = eps
         self._wclip = wclip
-        self._n_beta_annealing_steps = n_beta_annealing_steps
-        self._counter_for_beta = 0
+        self._default_priority = default_priority
+        self._default_non_zero_reward_priority = default_non_zero_reward_priority
+        self._default_done_priority = default_done_priority
 
         self._idx_sample = None 
         self._sumtree = None 
 
+    def _compute_default_priority(self, data):
+        priority = self._default_priority*np.ones_like(data['reward'], dtype=float)
+
+        if self._default_non_zero_reward_priority is not None:
+            priority[data['reward']!=0] = self._default_non_zero_reward_priority 
+
+        if self._default_done_priority is not None:
+            priority[data['done']==1] = self._default_done_priority
+
+        return priority
+
     def _smooth_and_warp_priority(self,priority):
-        p = (np.abs(priority)+self._eps)**self._alpha
-        return p
+        return (np.abs(priority)+self._eps)**self._alpha
 
     def append(self,data,priority=None):
         super(PrioritizedBufferMap,self).append(data)
 
         if priority is None:
-            priority = np.ones(self.first_dim_size,dtype=float)
+            priority = self._compute_default_priority(data)
 
         if self._sumtree is None:
             self._sumtree = PrioritizedSamplingBuffer(self._max_length)
@@ -72,18 +92,12 @@ class PrioritizedBufferMap(BufferMap):
             self.append(x,p)
 
     def sample(self,nsamples,beta=None,normalized=True):
+
+        if beta is None:
+            beta = 1.
+
         idx_sample = self._sumtree.sample(nsamples)
         output = {k:self._buffers[k].get(*idx_sample) for k in self._buffers}
-
-        # compute beta
-        self._counter_for_beta += 1
-        if beta is not None:
-            # beta has been passed directly to this sample function, so use that
-            pass
-        elif self._n_beta_annealing_steps is not None:
-            beta = min(1.,self._counter_for_beta / float(self._n_beta_annealing_steps))
-        else:
-            beta = 1.
 
         # sampled inverse priorities
         ipr_sample = 1./self._sumtree.get(*idx_sample)
@@ -122,4 +136,3 @@ class PrioritizedBufferMap(BufferMap):
 
     def priorities(self):
         return self._sumtree
-
