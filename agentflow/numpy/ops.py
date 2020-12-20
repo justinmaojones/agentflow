1c515850d234a3c1eb6cd6ef84c40aed8e41b7a8
Justin Mao-Jones <justinmaojones@gmail.com>
2020-12-20 10:34:35 -0800

    move numpy noise gen to numpy ops

--- agentflow/numpy/ops.py
+++ agentflow/numpy/ops.py
@@ -23,3 +23,26 @@ def moving_average(a, n=3):
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
+
+def clip(x,clip_to_min=None,clip_to_max=None):
+    if clip_to_min is not None:
+        x = np.maximum(x,clip_to_min)
+    if clip_to_max is not None:
+        x = np.minimum(x,clip_to_max)
+    return x
+
+def eps_greedy_noise(action_probs,eps=0.05):
+    if np.random.rand() < eps:
+        if action_probs.ndim == 1:
+            return np.random.choice(action_probs.shape[-1])
+        else:
+            return np.random.choice(action_probs.shape[-1],size=action_probs.shape[:-1])
+    else:
+        return action_probs.argmax(axis=-1)
+
+def gumbel_softmax_noise(action_probs,temperature=1.0,eps=1e-4):
+    action_probs = clip(action_probs,eps,1-eps)
+    annealed_logits = np.log(action_probs) / temperature
+    u = np.random.rand(*annealed_logits.shape)
+    g = -np.log(-np.log(u))
+    return (g+annealed_logits).argmax(axis=-1)
