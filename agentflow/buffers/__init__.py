17d40759542d11d197efcc3161749e4f087fc745
Justin Mao-Jones <justinmaojones@gmail.com>
2022-07-02 22:49:43 -0700

    n_step_return_buffer as flow

--- agentflow/buffers/__init__.py
+++ agentflow/buffers/__init__.py
@@ -3,5 +3,4 @@ from .buffer_map import BufferMap
 from .delayed_buffer_map import DelayedBufferMap
 from .nd_array_buffer import NDArrayBuffer
 from .n_step_return_buffer_map import NStepReturnBuffer
-from .n_step_return_buffer_map import NStepReturnPublisher
 from .prioritized_buffer_map import PrioritizedBufferMap
