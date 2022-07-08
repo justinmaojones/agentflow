5bc134f36aeea77974c62f8c9686021964593990
Justin Mao-Jones <justinmaojones@gmail.com>
2022-07-07 22:00:49 -0700

    migrate agentflow/agents/epsilon_greedy.py tests

--- agentflow/agents/epsilon_greedy.py
+++ agentflow/agents/epsilon_greedy.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 import tensorflow as tf
-import tensorflow_probability as tfp
 
 from agentflow.numpy.ops import eps_greedy_noise
 from agentflow.agents.flow import DiscreteActionAgentFlow
@@ -26,40 +25,3 @@ class EpsilonGreedy(DiscreteActionAgentFlow):
         if isinstance(greedy_action, tf.Tensor):
             greedy_action = greedy_action.numpy()
         return eps_greedy_noise(greedy_action, self.num_actions, self._get_eps())
-
-if __name__ == '__main__':
-    import numpy as np
-    import unittest
-    from unittest.mock import patch
-    from unittest.mock import MagicMock
-
-    from agentflow.agents.source import AgentSource
-
-    class Test(unittest.TestCase):
-
-        @patch('agentflow.agents.source.DiscreteActionAgentSource')
-        def test_act(self, MockAgentSource):
-            MockAgentSource.num_actions = 3
-            greedy_actions = tf.constant([0,1,2,0,1,2], dtype=tf.int64)
-            MockAgentSource.act = MagicMock(return_value = greedy_actions)
-            eps_greedy = EpsilonGreedy(source=MockAgentSource, epsilon=0.)
-            state = tf.zeros(6)
-            output = eps_greedy.act(state)
-            np.testing.assert_array_equal(greedy_actions.numpy(), output)
-
-        @patch('agentflow.agents.source.DiscreteActionAgentSource')
-        def test_act_exp_decay_schedule(self, MockAgentSource):
-            MockAgentSource.num_actions = 3
-            greedy_actions = tf.constant([0,1,2,0,1,2], dtype=tf.int64)
-            MockAgentSource.act = MagicMock(return_value = greedy_actions)
-            from agentflow.numpy.schedules.exponential_decay import ExponentialDecaySchedule
-            eps_schedule = ExponentialDecaySchedule(0.05, 0.0, 0.0)
-            eps_greedy = EpsilonGreedy(source=MockAgentSource, epsilon=eps_schedule)
-            state = tf.zeros(6)
-            output = eps_greedy.act(state)
-            output = eps_greedy.act(state)
-            np.testing.assert_array_equal(greedy_actions.numpy(), output)
-
-    unittest.main()
-
-
