3333fa9c858109190f07faebd3aad496134e6439
Justin Mao-Jones <justinmaojones@gmail.com>
2022-07-07 22:50:59 -0700

    update setup and add manifest and license

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from distutils.core import setup, Extension
+from setuptools import find_packages, setup, Extension
 
 setup(
 	name='agentflow',
 	version='1.0',
-	description='reinforcement learning project',
+	description='AgentFlow Reinforcement Learning Library',
+    url="https://github.com/justinmaojones/agentflow",
 	author='Justin Mao-Jones',
 	author_email='justinmaojones@gmail.com',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    include_package_data=True,
 	install_requires=open('requirements.txt').read(),
-	packages=['agentflow'],
+    packages=find_packages(include=["agentflow", "agentflow.*"]),
  )
