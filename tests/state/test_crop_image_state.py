2835007662c84c5a8ea11d66384701d33ee3329c
Justin Mao-Jones <justinmaojones@gmail.com>
2022-07-26 21:21:53 -0700

    add state tests

---
+++ tests/state/test_crop_image_state.py
@@ -0,0 +1,22 @@
+import numpy as np
+import unittest
+
+from agentflow.state.crop_image_state import CropImageState
+
+class TestCropImageState(unittest.TestCase):
+
+    def test_update(self):
+
+        state = CropImageState(top=1, bottom=1, left=1, right=1)
+        x = np.random.randn(3,4,5,6)
+        y = state.update(x)
+        np.testing.assert_array_equal(x[:,1:-1,1:-1,:], y)
+
+        for i in [1,2,3,5,6,7]:
+            x = np.random.randn(*[4]*i)
+            with self.assertRaises(ValueError):
+                state.update(x)
+
+if __name__ == '__main__':
+    unittest.main()
+
