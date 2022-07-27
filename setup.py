4bc7158b0426668a7a397df1c1e501d653a24e77
Justin Mao-Jones <justinmaojones@gmail.com>
2022-07-26 23:34:11 -0700

    add dev reqs and long descriptions

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
 from setuptools import find_packages, setup, Extension
 
+requirements = (open("requirements.txt").read(),)
+try:
+    requirements_dev = (open("requirements_dev.txt").read(),)
+    extras_require = {"dev": requirements_dev}
+except IOError:
+    extras_require = None
+
+
+def get_long_description():
+    # TODO: fix this hacky method of resolving relative links in pypi readme viewer
+    README = open("README.md").read()
+    base_url = "https://github.com/justinmaojones/agentflow/blob/master/"
+    relative_links = [
+        "docs/badges/python.svg",
+        "docs/badges/coverage.svg",
+    ]
+    for rlink in relative_links:
+        abs_link = base_url + rlink
+        README = README.replace(rlink, abs_link)
+    return README
+
+
 setup(
 	name='agentflow',
 	version='0.1',
 	description='AgentFlow Reinforcement Learning Library',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     url="https://github.com/justinmaojones/agentflow",
 	author='Justin Mao-Jones',
 	author_email='justinmaojones@gmail.com',
@@ -15,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3.9",
     ],
     include_package_data=True,
-	install_requires=open('requirements.txt').read(),
+    install_requires=requirements,
+    extras_require=extras_require,
     packages=find_packages(include=["agentflow", "agentflow.*"]),
  )
